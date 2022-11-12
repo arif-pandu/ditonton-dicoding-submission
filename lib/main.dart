00da38bb59a843de46115e4401cb617375da19a0
arif-pandu <mapanduprayoga@gmail.com>
2022-11-13 01:52:13 +0700

    Test new repo

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Ditonton',
         theme: ThemeData.dark().copyWith(
           colorScheme: kColorScheme,
           primaryColor: kRichBlack,

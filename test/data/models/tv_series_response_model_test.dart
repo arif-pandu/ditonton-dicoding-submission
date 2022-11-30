a6137ada388577d94c6f9c2aaebf824d20bdce2b
arif-pandu <mapanduprayoga@gmail.com>
2022-11-30 12:39:17 +0700

    Update : Finishing for submission again

--- test/data/models/tv_series_response_model_test.dart
+++ test/data/models/tv_series_response_model_test.dart
@@ -61,7 +61,6 @@ void main() {
                 "vote_average": 5.04,
                 "overview":
                     "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.",
-                "first_air_date": "2010-06-08",
                 "origin_country": ["US"],
                 "genre_ids": [18, 9648],
                 "original_language": "en",

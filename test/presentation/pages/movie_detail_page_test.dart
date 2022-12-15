3a6dabb316d14f4205ff3b3214aa4424fe27f80d
arif-pandu <mapanduprayoga@gmail.com>
2022-12-15 19:31:00 +0700

    Update : Refactoring test movie detail page

--- test/presentation/pages/movie_detail_page_test.dart
+++ test/presentation/pages/movie_detail_page_test.dart
@@ -28,35 +28,48 @@ void main() {
     mockMovieRecommendationBloc = MockMovieRecommendationBloc();
   });
 
+  void prepareInitStream() {
+    when(mockMovieDetailBloc.stream).thenAnswer((_) => Stream.empty());
+    when(mockMovieWatchlistBloc.stream).thenAnswer((_) => Stream.empty());
+    when(mockMovieRecommendationBloc.stream).thenAnswer((_) => Stream.empty());
+  }
+
+  void prepareInitState() {
+    when(mockMovieDetailBloc.state).thenReturn(MovieDetailInitial());
+    when(mockMovieWatchlistBloc.state).thenReturn(MovieWatchlistInitial());
+    when(mockMovieRecommendationBloc.state).thenReturn(MovieRecommendationInitial());
+  }
+
+  MultiBlocProvider buildRequiredWidget(MockMovieDetailBloc mockMovieDetailBloc,
+      MockMovieWatchlistBloc mockMovieWatchlistBloc, MockMovieRecommendationBloc mockMovieRecommendationBloc, int tId) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MovieDetailBloc>(
+          create: (_) => mockMovieDetailBloc,
+        ),
+        BlocProvider<MovieWatchlistBloc>(
+          create: (_) => mockMovieWatchlistBloc,
+        ),
+        BlocProvider<MovieRecommendationBloc>(
+          create: (_) => mockMovieRecommendationBloc,
+        ),
+      ],
+      child: MaterialApp(
+        home: MovieDetailPage(id: tId),
+      ),
+    );
+  }
+
   final tId = 1;
   testWidgets(
     'Initial Page',
     (WidgetTester tester) async {
-      when(mockMovieDetailBloc.stream).thenAnswer((_) => Stream.empty());
-      when(mockMovieWatchlistBloc.stream).thenAnswer((_) => Stream.empty());
-      when(mockMovieRecommendationBloc.stream).thenAnswer((_) => Stream.empty());
+      prepareInitStream();
 
-      when(mockMovieDetailBloc.state).thenReturn(MovieDetailInitial());
-      when(mockMovieWatchlistBloc.state).thenReturn(MovieWatchlistInitial());
-      when(mockMovieRecommendationBloc.state).thenReturn(MovieRecommendationInitial());
+      prepareInitState();
 
       await tester.pumpWidget(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider<MovieDetailBloc>(
-              create: (_) => mockMovieDetailBloc,
-            ),
-            BlocProvider<MovieWatchlistBloc>(
-              create: (_) => mockMovieWatchlistBloc,
-            ),
-            BlocProvider<MovieRecommendationBloc>(
-              create: (_) => mockMovieRecommendationBloc,
-            ),
-          ],
-          child: MaterialApp(
-            home: MovieDetailPage(id: tId),
-          ),
-        ),
+        buildRequiredWidget(mockMovieDetailBloc, mockMovieWatchlistBloc, mockMovieRecommendationBloc, tId),
       );
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
